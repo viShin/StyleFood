1c0c72b22625e0f6c20eef7c96ef4631be63b7dd
Shin <vi.shimada@gmail.com>
2019-01-05 01:31:57 -0200

    inicio do CRUD

--- api/server.js
+++ api/server.js
@@ -1,9 +1,26 @@
+//Constantes
 const express = require('express');
 const app     = express();
 
+//Get
 app.get('/', (req, res) =>{
     res.status(200).send("Hello World.");
 });
+
+//Post
+app.post('/', (req, res) =>{
+    res.status(201).send("Created.");
+});
+
+//Put
+app.put('/', (req, res) =>{
+    res.status(202).send("Updated.");
+})
+
+//Delete
+app.delete('/', (req, res) =>{
+    res.status(204).send("Deleted.");
+})
 
 //Inicia a API na porta digitada.
 app.listen(3000, ()=>{
