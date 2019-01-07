dd0588f9e23193e022e9180ba35a7eeb4d42e61b
Shin <vi.shimada@gmail.com>
2019-01-07 00:22:46 -0200

    conexao mongodb

--- api/bin/configuration/variables.js
+++ api/bin/configuration/variables.js
@@ -2,7 +2,11 @@ const variables = {
     Api: {
         //Utiliza a porta definida OU a 3000 caso não definido.
         port: process.env.port || 3000
+    },
+    Database: {
+        conn: process.env.connection || 'mongodb://admin:admin123@ds149984.mlab.com:49984/stylefood'
     }
+
 }
 
 module.exports = variables;
--- api/bin/express.js
+++ api/bin/express.js
@@ -1,6 +1,8 @@
 //Constantes
-const express        = require('express');
-const bodyPareser    = require('body-parser');
+const express     = require('express');
+const bodyPareser = require('body-parser');
+const mongoose    = require('mongoose');
+const variables   = require('../bin/configuration/variables');
 
 //Rotas
 const categoryRouter = require('../routes/category-router');
@@ -13,11 +15,12 @@ const app            = express();
 app.use(bodyPareser.json());
 app.use(bodyPareser.urlencoded({ extended: false }));
 
+//Config do Database
+mongoose.connect(variables.Database.conn);
+
 //Config das rotas
 app.use('/api/category', categoryRouter);
 app.use('/api/product', productRouter);
-
-
 
 //Export da API
 module.exports = app;
